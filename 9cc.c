13ee740cfa249caa187a27ac983a7670e35bafb4
tiro-finale <syubarutusiruto@icloud.com>
2020-01-25 21:52:49 +0900

    トークナイズ機能を追加

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdarg.h>
+
+typedef enum{
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // EOFトークン
+} TokenKind;
+
+typedef struct Token Token;
+struct Token{
+  TokenKind kind; // トークンの種類
+  Token *next;    // 次のトークン
+  int val;        // kind=TK_NUMの時の数値
+  char *str;      // トークン文字列
+};
+
+Token *token;
+
+// エラー用関数(使い方はprintfと同じ)
+void error(char *fmt, ...){
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// 次のトークンが期待される記号であればtrue
+bool consume(char op){
+  if(token->kind != TK_RESERVED || token->str[0] != op){
+    return false;
+  }
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待される記号であれば進める．
+// consumeとの違いは，エラーを出すかどうかである．
+void expect(char op){
+  if(token->kind != TK_RESERVED || token->str[0] != op){
+    printf("'%c'ではありません．\n", op);
+  }
+  token = token->next;
+}
+
+// 次のトークンが数字であればその数値を返す．
+int expect_number(){
+  if(token->kind != TK_NUM){
+    error("数ではありません");
+  }
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+// トークンがこれ以上続かないならtrue
+bool at_eof(){
+  return token->kind == TK_EOF;
+}
+
+// 次のトークンを生成する．
+Token *new_token(TokenKind kind, Token *cur, char *str){
+  Token *new = (Token*)calloc(1, sizeof(Token));
+  new->kind = kind;
+  new->str = str;
+  cur->next = new;
+  return new;
+}
+
+Token *tokenize(char *p){
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while(*p){
+
+    // 空白を無視する．
+    if(isspace(*p)){
+      p++;
+      continue;
+    }
+
+    if(*p == '+' || *p == '-'){
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if(isdigit(*p)){
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error("トークナイズできない文字です．");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
+}
 
 int main(int argc, char **argv){
 
@@ -8,28 +110,34 @@ int main(int argc, char **argv){
     return 1;
   }
 
-  char *p = argv[1];
+  token = tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while(*p){
-    if(*p == '+'){
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-    if(*p == '-'){
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+  // 最初は数値である．
+  printf("  mov rax, %d\n", expect_number());
+
+  // '+ 数値'或いは'- 数値'というトークンを読み取る．
+  while(!at_eof()){
+    // +かな?
+    if(consume('+')){
+      printf("  add rax, %d\n", expect_number());
       continue;
     }
 
-    fprintf(stderr, "予期しない文字です．: '%c'\n", *p);
-    return 1;
+    // +じゃなければ当然-だよね?
+    /*
+    if(consume('-')){
+      printf("  sub rax, %d\n", expect_number());
+      continue;
+    }
+    error("数ではありません．");
+    */
+    expect('-');
+    printf("  sub rax, %d\n", expect_number());
   }
 
   printf("  ret\n");
