cdec220c74c276211ee34b8238dd60e0d17778e7
tiro-finale <syubarutusiruto@icloud.com>
2020-02-16 19:42:05 +0900

    Macでもコンパイルできるように命令を変更した．

--- codegen.c
+++ codegen.c
@@ -102,22 +102,22 @@ void generate(Node *node){
     case ND_EQ:
       printf("  cmp rax, rdi\n");
       printf("  sete al\n");
-      printf("  movzb rax, al\n");
+      printf("  movzx rax, al\n");
       break;
     case ND_NE:
       printf("  cmp rax, rdi\n");
       printf("  setne al\n");
-      printf("  movzb rax, al\n");
+      printf("  movzx rax, al\n");
       break;
     case ND_LT:
       printf("  cmp rax, rdi\n");
       printf("  setl al\n");
-      printf("  movzb rax, al\n");
+      printf("  movzx rax, al\n");
       break;
     case ND_LE:
       printf("  cmp rax, rdi\n");
       printf("  setle al\n");
-      printf("  movzb rax, al\n");
+      printf("  movzx rax, al\n");
       break;
     default:
       break;
@@ -130,9 +130,9 @@ void generate(Node *node){
 
 void codegen(Node *node){
   printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
+  printf(".global _main\n");
   printf("\n");
-  printf("main:\n");
+  printf("_main:\n");
 
   printf("  push rbp\n");
   printf("  mov rbp, rsp\n");
